2b0284c2c319aa1a0df2dbb3580d769997e7ff04
antongigele <anton.gigele@aon.at>
2023-11-16 17:32:28 +0100

    file change

--- main/src/spell_checker.py
+++ main/src/spell_checker.py
@@ -1,13 +1,28 @@
 from nltk import edit_distance
 from nltk import word_tokenize, sent_tokenize
+from nltk.corpus import stopwords, words
 import nltk
 import pandas as pd
 
-# sentence = "yuo call"
+print(words)
+
+# sentence = "yuo call it a lax"
+
+# queries_df = pd.read_csv("C:\Users\Anton\Desktop\projects\spell_checker\csv_imports\nlp_queries.csv")
+
+path = "C:\Users\Anton\Desktop\projects\spell_checker\csv_imports\nlp_queries.csv"
+
+def load_and_preprocess_words(path):
+    queries_df = pd.read_csv(path)
+    
+    return queries_df.drop_duplicates(subset="query")["query"]
+
+
 
 word_list = [
     "work",
     "call",
+    "cali",
     "man",
     "woman",
     "use",
@@ -36,6 +51,9 @@ def get_closest_sentence(query_string):
             distance_dict[word] = edit_distance(term, word)
         closest_terms[term] = distance_dict
 
-    print(min(closest_terms[tokenized_sentence[0]], key=closest_terms[tokenized_sentence[0]].get), min(closest_terms[tokenized_sentence[1]], key=closest_terms[tokenized_sentence[1]].get))
+    result_list = [min(closest_terms[tokenized_sentence[i]], key=closest_terms[tokenized_sentence[i]].get) for i in range(len(tokenized_sentence))]
 
-queries_df = pd.read_csv("../csv_imports/nlp_queries.csv")
+    return result_list
+    # return min(closest_terms[tokenized_sentence[0]], key=closest_terms[tokenized_sentence[0]].get), min(closest_terms[tokenized_sentence[1]], key=closest_terms[tokenized_sentence[1]].get))
+
+# print(get_closest_sentence(sentence))
