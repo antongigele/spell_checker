5f52c6afdb2805819c91b530bd7ae1b422ad3ecc
antongigele <anton.gigele@aon.at>
2023-11-28 00:09:55 +0100

    initial commit

---
+++ lib/read_write.py
@@ -0,0 +1,12 @@
+import json
+import csv
+
+with open('/Users/antongigele/Desktop/python/spell_checker/csv_imports/porn_acronyms.json') as json_file:
+    acronyms_dict = json.load(json_file)
+
+with open("/Users/antongigele/Desktop/python/spell_checker/csv_imports/atoms.csv", "w", newline="") as outfile: 
+    write = csv.writer(outfile)
+    write.writerow(list())
+
+with open("/Users/antongigele/Desktop/python/spell_checker/csv_imports/porn_acronyms.json", "w") as outfile: 
+        json.dump(source, outfile)
