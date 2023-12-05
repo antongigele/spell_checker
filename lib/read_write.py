1fa1af0280a1ccaeefa3a6649405ddb0f3ef5d06
antongigele <anton.gigele@aon.at>
2023-12-05 20:48:10 +0100

    comments add

--- lib/read_write.py
+++ lib/read_write.py
@@ -1,9 +1,11 @@
 import json
 import csv
 
+### read
 with open('/Users/antongigele/Desktop/python/spell_checker/csv_imports/porn_acronyms.json') as json_file:
     acronyms_dict = json.load(json_file)
 
+### write
 with open("/Users/antongigele/Desktop/python/spell_checker/csv_imports/atoms.csv", "w", newline="") as outfile: 
     write = csv.writer(outfile)
     write.writerow(list())
